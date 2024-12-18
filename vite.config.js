c463e916416f199ab7db65f222cf0594f892c364
Paulo Wells <paulowells@gmail.com>
2024-12-18 12:08:48 -0300

    updated resources entry

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/index.css',
-                'resources/scss/main.scss',
+                'resources/sass/main.scss',
                 'resources/js/app.js'
             ],
             refresh: true,
